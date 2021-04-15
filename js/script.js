aa2a10882ab6971bae9835a0a994a210c48e4b6c
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-04-15 15:23:23 +0200

    request fix

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ var app = new Vue({
   },
   methods: {
     searchMovie: function() {
-      axios.get('https://api.themoviedb.org/3/search/movie?', {
+      axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: "50ab899d8790694ff25d85dc8549540d",
             query: this.title,
@@ -23,18 +23,16 @@ var app = new Vue({
         })
         .then((response) => {
           this.films = response.data.results;
+          axios.get('https://api.themoviedb.org/3/search/tv', {
+            params: {
+              api_key: "50ab899d8790694ff25d85dc8549540d",
+              query: this.title,
+              language: "it-IT",
+            }
+          }).then((response) => {
+            this.films = [...this.films, ...response.data.results];
+          });
         });
-
-      axios.get('https://api.themoviedb.org/3/search/tv?', {
-        params: {
-          api_key: "50ab899d8790694ff25d85dc8549540d",
-          query: this.title,
-          language: "it-IT",
-        }
-      }).then((response) => {
-        this.films = [...this.films, ...response.data.results];
-      });
     },
-
   }
 });
